afe9d132dc186595c90b6352d6b05d217e7d3b08
Leonardo Gonzalez <gonzalezleonardo042@gmail.com>
2025-10-30 18:08:45 -0300

    Agregar rutas para el dashboard y la suma, incluyendo middleware de autenticación

--- Proyecto1/routes/web.php
+++ Proyecto1/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::get('/inicio', function () {
+    return view('inicio');
+});
+
+Route::post('/suma', function (Request $request) {
+    $num1 = $request->input('num1');
+    $num2 = $request->input('num2');
+    $resultado = $num1 + $num2;
+    
+    return view('suma', ['resultado' => $resultado]);
+
 });
